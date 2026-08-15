e82b0eb360c01d9f0790d41b72813c0e0eb56416
Centos8 <shuhe@localhost.localdomain>
2026-08-15 02:54:15 -0700

    添加 TcpServer类和 TcpConnection类

---
+++ muduo/Callbacks.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <memory>
+#include <functional>
+
+class Buffer;
+class TcpConnection;
+class Timestamp;
+
+typedef std::shared_ptr<TcpConnection> TcpConnectionPtr;
+typedef std::function<void(const TcpConnectionPtr&)> ConnectionCallback;
+typedef std::function<void(const TcpConnectionPtr&)> CloseCallback;
+typedef std::function<void(const TcpConnectionPtr&)> WriteCompleteCallback;
+typedef std::function<void(const TcpConnectionPtr&, Buffer*, Timestamp)> MessageCallback;
+typedef std::function<void(const TcpConnectionPtr&, size_t)> HighWaterMarkCallback;
