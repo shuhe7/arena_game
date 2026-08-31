8d50d39180502f4769e5855e046bad883e8f5227
shuhe7 <3316222035@qq.com>
2026-08-31 14:29:01 +0800

    加上登录页面和认证流程

--- client/ArenaClient/mainwindow.cpp
+++ client/ArenaClient/mainwindow.cpp
@@ -1,14 +1,54 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "network/GameClient.h"
+#include "scenes/LoginScene.h"
+
+#include <QStackedWidget>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
 {
-    ui->setupUi(this);
+    initNetwork();
+    initUi();
 }
 
-MainWindow::~MainWindow()
+MainWindow::~MainWindow() = default;
+
+void MainWindow::switchTo(Scene scene)
 {
-    delete ui;
+    switch (scene)
+    {
+    case SCENE_LOGIN:
+        stack_->setCurrentWidget(loginScene_);
+        break;
+
+    case SCENE_LOBBY:
+    case SCENE_BATTLE:
+    case SCENE_RESULT:
+        break;
+    }
+}
+
+void MainWindow::setUserInfo(uint32_t uid, const QString &name)
+{
+
+}
+
+void MainWindow::initUi()
+{
+    stack_ = new QStackedWidget(this);
+
+    loginScene_ = new LoginScene(this, stack_);
+    stack_->addWidget(loginScene_);
+
+    setCentralWidget(stack_);
+    setWindowTitle("Arena PvP");
+    resize(960, 600);
+
+    switchTo(SCENE_LOGIN);
+}
+
+void MainWindow::initNetwork()
+{
+    client_ = std::make_unique<GameClient>();
 }
--- client/ArenaClient/mainwindow.h
+++ client/ArenaClient/mainwindow.h
@@ -1,23 +1,55 @@
-#ifndef MAINWINDOW_H
-#define MAINWINDOW_H
-
+#pragma once
 #include <QMainWindow>
+#include <QStackedWidget>
+#include <QTimer>
+#include <memory>
 
-QT_BEGIN_NAMESPACE
-namespace Ui {
-class MainWindow;
-}
-QT_END_NAMESPACE
+class LoginScene;
+class LobbyScene;
+class BattleScene;
+class ResultScene;
+class GameClient;
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
-
 public:
-    MainWindow(QWidget *parent = nullptr);
+    enum Scene
+    {
+        SCENE_LOGIN,
+        SCENE_LOBBY,
+        SCENE_BATTLE,
+        SCENE_RESULT,
+    };
+
+    explicit MainWindow(QWidget* parent = nullptr);
     ~MainWindow();
 
+    void switchTo(Scene scene);
+
+    // Shared state
+    void setUserInfo(uint32_t uid, const QString& name);
+
+    uint32_t    userId() const { return userId_; }
+    QString     username() const { return username_; }
+
+    GameClient* getClient() { return client_.get(); }
+
+signals:
+    void userLoggedIn(uint32_t uid, const QString& name);
+
 private:
-    Ui::MainWindow *ui;
+    void initUi();
+    void initNetwork();
+
+    QStackedWidget* stack_ = nullptr;
+    LoginScene*     loginScene_ = nullptr;
+    LobbyScene*     lobbyScene_ = nullptr;
+    BattleScene*    battleScene_ = nullptr;
+    ResultScene*    resultScene_ = nullptr;
+
+    std::unique_ptr<GameClient> client_;
+
+    uint32_t userId_ = 0;
+    QString  username_;
 };
-#endif // MAINWINDOW_H
